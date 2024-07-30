90a33fb644dbb8b069bca087666e3dc479027144
David  Pérez <56732148+DavidPerezz99@users.noreply.github.com>
2024-07-30 06:23:01 -0500

    Update and rename deployment_test.py to deployment_test.py

--- tests/deployment_test.py
+++ tests/deployment_test.py
@@ -1,19 +1,31 @@
 import asyncio
 import httpx
 import time
+from tests.client import get_jwt_token
+import json 
 
-URL = "http://127.0.0.1:8000/predict"  
 
-async def send_request(client, data):
+URL = "http://127.0.0.1:8000" 
+auth_data = {
+    "username": "CPS_DAVID_PEREZ_DEV",
+    "password": "UIS@123FCV"
+}
+with open('input_test.json') as f:
+    data = json.load(f)
+token = get_jwt_token(URL + "/auth/token", auth_data)
+headers = {'Authorization': f'Bearer {token}'}
+
+
+async def send_request(client, data, headers):
     try:
-        response = await client.post(URL, json=data)
+        response = await client.post(URL + "/predict", json=data, headers = headers)
         return response.status_code, response.json()
     except Exception as e:
         return None, str(e)
 
-async def main(num_requests, data):
+async def main(num_requests, data, headers):
     async with httpx.AsyncClient() as client:
-        tasks = [send_request(client, data) for _ in range(num_requests)]
+        tasks = [send_request(client, data, headers) for _ in range(num_requests)]
         start_time = time.time()
         responses = await asyncio.gather(*tasks)
         end_time = time.time()
@@ -26,13 +38,5 @@ async def main(num_requests, data):
         print(f"Failed responses: {len(failed_responses)}")
         print(f"Total time taken: {end_time - start_time} seconds")
 
-
-test_data = {
-    "feature1": "value1",
-    "feature2": "value2",
-
-}
-
 num_requests = 1000
-
-asyncio.run(main(num_requests, test_data))
+asyncio.run(main(num_requests, data, headers))
