3ed09677037b55302c7448404fa02dabe4960bf7
David  Pérez <56732148+DavidPerezz99@users.noreply.github.com>
2024-08-01 11:40:52 -0500

    Update client.py

--- app/tests/client.py
+++ app/tests/client.py
@@ -2,7 +2,8 @@ import requests
 import json
 
 # Base URL of the local app
-base_url = "http://127.0.0.1:8000"
+base_url = "http://172.30.19.97:5001"
+predict_url = "http://172.30.19.97:5001"
 
 # Load the input data for the predict endpoint
 with open('input_test.json') as f:
@@ -10,16 +11,18 @@ with open('input_test.json') as f:
 
 # Authentication credentials
 auth_data = {
-    "username": "CPS_DAVID_PEREZ_DEV",
-    "password": "UIS@123FCV"
+    "username": "user",
+    "password": "pass"
 }
 
 def get_jwt_token(auth_url, auth_data):
     response = requests.post(auth_url, json=auth_data)
-    if response.status_code == 200:
-        return response.json().get('access_token')
-    else:
-        raise Exception("Authentication failed")
+    print(response.json().get('access_token'))
+    return response.json().get('access_token')
+    #if response.status_code == 200:
+    #    return response.json().get('access_token')
+    #else:
+    #    raise Exception("Authentication failed")
 
 # Endpoints
 endpoints = {
@@ -30,12 +33,11 @@ endpoints = {
 # Test each endpoint
     # Get JWT token
 token = get_jwt_token(base_url + "/auth/token", auth_data)
-headers = {'Authorization': f'Bearer {token}'}
-response = requests.post(base_url+ "/predict", json=input_data, headers=headers)
+headers = {'token':token}
+response = requests.post(predict_url+ "/predict", json=input_data)
 #else:
     #response = requests.get(url)
     
 print(f"Status Code: {response.status_code}")
 print(f"Response: {response.json()}")
 print("="*50)
-
