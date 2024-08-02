5a9c59defe7f9b7743b512ed839f0857e2adb078
David  Pérez <56732148+DavidPerezz99@users.noreply.github.com>
2024-08-02 00:23:33 -0500

    Update auth.py

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -7,20 +7,23 @@ from sqlalchemy.orm import Session
 from starlette import status
 from app.database import SessionLocal
 from app.utils.models import Users
-# from models import Users
+#from models import Users
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import jwt, JWTError
 
 print(sqlalchemy.__version__)
 
-router = APIRouter(prefix="/auth", tags=["auth"])
+router = APIRouter(
+    prefix = '/auth',
+    tags=['auth']
+)
 
-SECRET_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTcxODA2Mjk0NCwiaWF0IjoxNzE4MDYyOTQ0fQ.zN9eemsiMb7rGanbHVXumbU5wHJDnDBYg3jp8WoRaAg"
-ALGORITHM = "HS256"
+SECRET_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTcxODA2Mjk0NCwiaWF0IjoxNzE4MDYyOTQ0fQ.zN9eemsiMb7rGanbHVXumbU5wHJDnDBYg3jp8WoRaAg'
+ALGORITHM = 'HS256'
 
-bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
+bcrypt_context = CryptContext(schemes=['bcrypt'],deprecated='auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 
 class CreateUserRequest(BaseModel):
@@ -38,23 +41,21 @@ def get_db():
     try:
         yield db
 
-    finally:
+    finally: 
         db.close()
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-
-@router.post("/create_user", status_code=status.HTTP_201_CREATED)
+@router.post("/create_user",status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency,
                       create_user_request: CreateUserRequest):
     create_user_model = Users(
         username=create_user_request.username,
-        hashed_password=bcrypt_context.hash(create_user_request.password),
+        hashed_password=bcrypt_context.hash(create_user_request.password)
     )
     db.add(create_user_model)
     db.commit()
-
 
 @router.post("/token", response_model=Token)
 async def login_for_access_token(
@@ -72,34 +73,28 @@ async def login_for_access_token(
 
 
 def authenticate_user(username: str, password: str, db):
-    user = db.query(Users).filter(Users.username == username).first()
-    if not user:
+    user = db.query(Users).filter(Users.username==username).first()
+    if not user: 
         return False
     if not bcrypt_context.verify(password, user.hashed_password):
         return False
     return user
 
-
 def create_access_token(username: str, user_id: int, expires_delta: timedelta):
-    encode = {"sub": username, "id": user_id}
-    expires = datetime.utcnow() + expires_delta
-    encode.update({"exp": expires})
-    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+    encode = {'sub':username, 'id':user_id}
+    expires = datetime.utcnow()+expires_delta
+    encode.update({'exp':expires})
+    return jwt.encode(encode,SECRET_KEY,algorithm=ALGORITHM)
 
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        user_id: int = payload.get("id")
-        if username is None or user_id is None:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Could not validate user.",
-            )
-        return {"username": username, "id": user_id, "token": token}
-    except JWTError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate user."
-        )
+async def get_current_user(token: Annotated[str,Depends(oauth2_bearer)]):
+    try: 
+        payload = jwt.decode(token,SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+        if username is None or user_id is None: 
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail='Could not validate user.')
+        return {'username':username, 'id':user_id}
+    except JWTError: 
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Could not validate user.')
