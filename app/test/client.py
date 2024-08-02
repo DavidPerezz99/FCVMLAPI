3a24d78c660c01fc67adacf6bcc5e888debe11b6
David  Pérez <56732148+DavidPerezz99@users.noreply.github.com>
2024-08-02 00:27:39 -0500

    Update client.py

--- app/test/client.py
+++ app/test/client.py
@@ -1,43 +1,47 @@
 import requests
 import json
-from fastapi.security import OAuth2PasswordRequestForm  # type: ignore
 
-
+# Base URL of the local app
 base_url = "http://127.0.0.1:8000"
-predict_url = "http://localhost:5000"
+predict_url = "http://127.0.0.1:8000"
 
-# python app/test/client.py
-
-with open("app/test/input_test.json") as f:
+# Load the input data for the predict endpoint
+with open('input_test.json') as f:
     input_data = json.load(f)
 
 # Authentication credentials
-# form_data = OAuth2PasswordRequestForm(username="user", password="pass")
-form_data = {"username": "user", "password": "pass"}
-
+auth_data = {
+    "username": "CPS_DAVID_PEREZ_DEV",
+    "password": "UIS@123FCV"
+}
 
 def get_jwt_token(auth_url, auth_data):
-    try:
-        response = requests.post(auth_url,
-                                 data=auth_data,
-                                 headers={"Content-Type": "application/x-www-form-urlencoded"})
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        return None
+    response = requests.post(auth_url, data=auth_data)
+    print(response.json().get('access_token'))
+    return response.json().get('access_token')
+    #if response.status_code == 200:
+    #    return response.json().get('access_token')
+    #else:
+    #    raise Exception("Authentication failed")
 
+# Endpoints
+endpoints = {
+    "login": "/",
+    "predict": "/predict"
+}
 
-token_data = get_jwt_token(base_url + "/auth/token", form_data)
-print(token_data)
+# Test each endpoint
+    # Get JWT token
+token = get_jwt_token(base_url + "/auth/token", auth_data)
+headers = {'token':token}
+response = requests.post(predict_url+ "/predict", json=input_data, headers = headers)
+#else:
+    #response = requests.get(url)
+    
+print(f"Status Code: {response.status_code}")
+print(f"Response: {response.json()}")
+print("="*50)
 
-if token_data:
-    token = token_data.get("access_token")
-    headers = {"Authorization": f"Bearer {token}"}
-    response = requests.post(base_url + "/predict",
-                             json=input_data,
-                             headers=headers)
+response_validate_user = requests.get(predict_url+ "/", json=headers)
+print(f"Active User is: {response_validate_user.json()}")
 
-    print(f"Status Code: {response.status_code}")
-    print(f"Response: {response.json()}")
-else:
-    print("Failed to obtain JWT token.")
