fe8bc14716c6cbbeb3c4cb9378ac7079f478adad
David  Pérez <56732148+DavidPerezz99@users.noreply.github.com>
2024-10-02 08:51:24 -0500

    Update deployment_test.py
    Unit test cases added.

--- app/tests/deployment_test.py
+++ app/tests/deployment_test.py
@@ -6,9 +6,9 @@ import json
 
 
 # URL = "http://127.0.0.1:8000"
-base_url = "http://192.168.73.37:5000"
+base_url = "http://172.30.19.97:5000"
 # predict_url = "http://127.0.0.1:5001"
-predict_url = "http://192.168.73.37:5001"
+predict_url = "http://172.30.19.97:5001"
 
 auth_data = {"username": "CPS_DAVID_PEREZ_DEV", "password": "UIS@123FCV"}
 with open("input_test.json") as f:
@@ -29,22 +29,29 @@ async def send_request(client, data, headers, timeout):
 
 async def main(num_requests, data, headers, timeout):
     async with httpx.AsyncClient() as client:
-        tasks = [send_request(client, data, headers, timeout) for _ in range(num_requests)]
         start_time = time.time()
+        tasks = [send_request(client, data, headers, timeout) for _ in range(num_requests)]
         responses = await asyncio.gather(*tasks)
         end_time = time.time()
-
+        
         success_responses = [r for r in responses if r[0] == 200]
         failed_responses = [r for r in responses if r[0] is None or r[0] != 200]
-        all_responses = [r for r in responses]
-
-        print("All responses:", all_responses)
+        
         print(f"Total requests: {num_requests}")
         print(f"Successful responses: {len(success_responses)}")
         print(f"Failed responses: {len(failed_responses)}")
         print(f"Total time taken: {end_time - start_time} seconds")
+    return responses
 
+num_requests = 1000
+timeout = 300;
+responses = asyncio.run(main(num_requests, data, headers, timeout))
 
-num_requests = 500
-timeout = 30  # seconds
-asyncio.run(main(num_requests, data, headers, timeout))
+# Extract the JSON parts of the successful responses
+json_responses = [r[1] for r in responses]
+
+# Save to a JSON file
+with open('responses.json', 'w') as f:
+    json.dump(json_responses, f, indent=4)
+
+print("Responses have been saved to 'responses.json'")
